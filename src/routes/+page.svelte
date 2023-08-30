c9ea4533416667dfe8cc6930eeeb811dd79ed959
Mateus Lopes <mateusyasmim2014@gmail.com>
2023-08-30 20:11:43 -0300

    Primeiro commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { row, col } from '../store.js'
+  import { goto } from '$app/navigation';
+  
+  function startGame(){
+    goto('game')
+  }
+</script>
+
+<label for="">Row</label>
+<input bind:value={$row} />
+<label for="">Column</label>
+<input bind:value={$col} />
+<button on:click={() => startGame($row, $col)}>Start Game</button>
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { writable } from 'svelte/store';
+
+export const row = writable(0);
+export const col = writable(0);
